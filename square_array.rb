49eff587a2a8d9bc5e4f2e2f81a617cb99ee179a
Renee Sims <info@reneesoul.com>
2019-07-18 03:06:44 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
   # your code here
+  
+  square_these = array
+  new_squares = []
+  count = 0
+  
+  square_these.each do |square|
+    new_squares[count] = square ** 2
+    count += 1
+  end
+# puts square_these
+
+
+puts "The squared values of an array containing the numbers (i.e. 1, 2, & 3), are assigned to a new array resulting in the values: #{new_squares}."  
+return new_squares
+
 end
