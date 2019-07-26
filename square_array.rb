9d53bd74f982e0c6231b86461d4ea5873f186f01
assane savadogo <assane.savadogo81@gmail.com>
2019-07-26 02:17:52 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -3,11 +3,8 @@ def square_array(array)
   new_arr = []
   array.each do |num| 
     new_arr << num ** 2 
+     #binding.pry
   end 
   new_arr
 end
 
-def square_array(array)
-  array.collect {|num| num ** 2}
-  binding.pry
-end
